c6e78a2aa595b1fc2d010f9c380b32cd15f7524a
Zephyrif <rifdamusyaffa007@gmail.com>
2024-02-28 00:11:10 +0700

    P3_Tugas_No3_23

--- src/P3/Tugas2/MahasiswaMain.java
+++ src/P3/Tugas2/MahasiswaMain.java
@@ -4,8 +4,10 @@ import java.util.Scanner;
 public class MahasiswaMain {
     public static void main(String[] args) {
         Scanner sc23 = new Scanner(System.in);
+        int n = 3;
+        double[] daftarIPK = new double[n];
 
-        Mahasiswa[] mahasiswa = new Mahasiswa[3];
+        Mahasiswa[] mahasiswa = new Mahasiswa[n];
 
         for (int i = 0, j = i; i < 3; i++) {
             System.out.println("Masukkan data mahasiwa ke-" + ++j + " : ");
@@ -17,6 +19,7 @@ public class MahasiswaMain {
             String kelamin = sc23.next();
             System.out.print("Masukkan IPK: ");
             double ipk = sc23.nextDouble();
+            daftarIPK[i] = ipk;
             mahasiswa[i] = new Mahasiswa(nama, nim, kelamin, ipk);
         }
 
@@ -28,6 +31,31 @@ public class MahasiswaMain {
             System.out.println("Jenis Kelamin: " + mahasiswa[i].kelamin);
             System.out.println("IPK: " + mahasiswa[i].ipk);
         }
+
+        double rataRataIPK = rataRataIPK(n, daftarIPK);
+        System.out.println();
+        System.out.printf("%-10s %-2.2f", "Rata-rata IPK mahasiswa : ", rataRataIPK);
+        System.out.println();
+        IPKTerbesar(mahasiswa);
         sc23.close();
+    }
+
+    public static double rataRataIPK(int jumlah, double[] IPK) {
+        double[] dataIPK = new double[jumlah];
+        for (int i = 0; i < jumlah; i++) {
+            dataIPK[i] = IPK[i];
+        }
+        double rataIPK = (dataIPK[0] + dataIPK[1] + dataIPK[2]) / jumlah;
+        return rataIPK;
+    }
+
+    public static void IPKTerbesar(Mahasiswa[] mahasiswa) {
+        if (mahasiswa[0].ipk > mahasiswa[1].ipk && mahasiswa[0].ipk > mahasiswa[2].ipk){
+            System.out.println("Mahasiswa: " + mahasiswa[0].nama + ", IPK: " + mahasiswa[0].ipk);
+        } else if (mahasiswa[1].ipk > mahasiswa[0].ipk && mahasiswa[1].ipk > mahasiswa[2].ipk) {
+            System.out.println("Mahasiswa: " + mahasiswa[1].nama + ", IPK: " + mahasiswa[1].ipk);
+        }else if (mahasiswa[2].ipk > mahasiswa[0].ipk && mahasiswa[2].ipk > mahasiswa[1].ipk) {
+            System.out.println("Mahasiswa: " + mahasiswa[2].nama + ", IPK: " + mahasiswa[2].ipk);
+        }
     }
 }
