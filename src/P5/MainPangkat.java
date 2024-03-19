94ddf7168d3a0de55d151a9e5958192eb1c91220
Zephyrif <rifdamusyaffa007@gmail.com>
2024-03-19 15:58:12 +0700

    P5_Percobaan2_23

---
+++ src/P5/MainPangkat.java
@@ -0,0 +1,34 @@
+package P5;
+
+import java.util.Scanner;
+
+public class MainPangkat {
+    public static void main(String[] args) {
+        Scanner sc23 = new Scanner(System.in);
+        System.out.println("================");
+        System.out.print("Masukkan Jumlah Element Yang Ingin Dihitung: ");
+        int element = sc23.nextInt();
+
+        Pangkat[] png = new Pangkat[element];
+        for (int i = 0; i < png.length; i++) {
+            png[i] = new Pangkat();
+            System.out.print("Masukkan nilai yang ingin dipangkatkan: ");
+            int nilai = sc23.nextInt();
+            System.out.print("Masukkan nilai pemangkat: ");
+            int pangkat = sc23.nextInt();
+            png[i].nilai = nilai;
+            png[i].pangkat = pangkat;
+        }
+
+        System.out.println("HASIL  PANGKAT- BRUTE FORCE");
+        for (int i = 0; i < png.length; i++){
+            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatBF(png[i].nilai, png[i].pangkat));
+        }
+
+        System.out.println("HASIL PANGKAT - DIVIDE CONQUER");
+        for (int i = 0; i < png.length;i++){
+            System.out.println("Hasil dari " + png[i].nilai + " pangkat " + png[i].pangkat + " adalah " + png[i].pangkatDC(png[i].nilai, png[i].pangkat));
+        }
+        sc23.close();
+    }
+}
