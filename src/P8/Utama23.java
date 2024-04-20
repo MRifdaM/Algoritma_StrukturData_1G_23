0f407bdd45236d491a9f8640a4ba28a2ad995188
Zephyrif <rifdamusyaffa007@gmail.com>
2024-04-20 22:10:44 +0700

    P8_Percobaan1_Pertanyaan4_23

--- src/P8/Utama23.java
+++ src/P8/Utama23.java
@@ -4,15 +4,20 @@ import java.util.Scanner;
 
 public class Utama23 {
     public static void main(String[] args) {
-        Gudang23 gudang = new Gudang23(7);
         Scanner sc23 = new Scanner(System.in);
+
+        System.out.print("Masukkan kapasitas Gudang : ");
+        int n = sc23.nextInt();
+
+        Gudang23 gudang = new Gudang23(n);
 
         while (true) {
             System.out.println("\nMenu");
             System.out.println("1. Tambah barang");
             System.out.println("2. Ambil barang");
             System.out.println("3. Tampilkan tumpukan barang");
-            System.out.println("4. Keluar");
+            System.out.println("4. Lihat Barang Teratas");
+            System.out.println("5. Keluar");
             System.out.print("Pilih Operasi : ");
             int menu = sc23.nextInt();
 
@@ -38,6 +43,10 @@ public class Utama23 {
                 break;
 
                 case 4:
+                gudang.lihatBarangTeratas();
+                break;
+
+                case 5:
                 sc23.close();
                 break;
 
